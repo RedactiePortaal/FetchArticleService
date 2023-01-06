b075fae99495157f2b99f600db66ec56694eec7a
Niek <niek@openmaze.io>
2023-01-06 13:40:03 +0100

    more cleanups...

--- src/articles/articles.service.ts
+++ src/articles/articles.service.ts
@@ -14,35 +14,44 @@ export class ArticlesService {
     new NOSAdaptee(),
   ];
 
+  // Method for retrieving all articles from a specific source, with a given interval
   async findWithInterval(interval: number, source: number): Promise<void> {
     const adapter = new RSSAdapter(this.dataProviders[source]);
     const articles: Article[] = await adapter.adaptee.findWithInterval(
       interval,
     );
+
+    // Send each element seperately to prevent flooding the articleprocessor
     await articles.forEach((element) => this.sendToQueue(element));
     return;
   }
 
+  // Method for retrieving all articles from a specific source
   async findAll(source: number): Promise<void> {
     const adapter = new RSSAdapter(this.dataProviders[source]);
     const articles: Article[] = await adapter.adaptee.findAll();
+
+    // Send each element seperately to prevent flooding the articleprocessor
     await articles.forEach((element) => this.sendToQueue(element));
     return;
   }
 
+  // Method for sending an article to the articleprocessor
   async sendToQueue(article: Article): Promise<void> {
+    // Check if article is undefined
+    // SHOULD THIS BE DONE EARLIER? Probably, yes.
     if (article == undefined) {
       console.log('Article is undefined');
       return;
     }
+
+    // Send the article to the articleprocessor
     await axios
       .post('http://localhost:1880/article/process', article)
-      .then(function (response) {
-        // console.log(response.status);
-      })
       .catch(function (error) {
         return;
       });
+    // No return needed, articles are sent to the articleprocessor
     return;
   }
 }
--- src/articles/dto/parsed-article.dto.ts
+++
@@ -1,9 +0,0 @@
-export interface ParsedArticleDTO {
-  title: string;
-  location: string;
-  description: string;
-  image: string | null;
-  category: string | null;
-  link: string;
-  pubDate: Date;
-}

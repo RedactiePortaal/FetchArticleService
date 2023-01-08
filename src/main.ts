a95c4895383dc47cad056d689d4457f9b7aba7ae
RubenFricke <rubenfricke@gmail.com>
2023-01-08 21:53:55 +0100

    Add DOTENV

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as dotenv from 'dotenv';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,4 +16,5 @@ async function bootstrap() {
 
   await app.listen(3000);
 }
+dotenv.config();
 bootstrap();

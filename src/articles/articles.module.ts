3307389082fa1c2b499c1a7d8b2d84b197a26bb8
RubenFricke <rubenfricke@gmail.com>
2023-01-08 20:30:02 +0100

    Add adaptees to providers in module

--- src/articles/articles.module.ts
+++ src/articles/articles.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { ArticlesService } from './articles.service';
 import { ArticlesController } from './articles.controller';
 import { HttpModule } from '@nestjs/axios';
+import { FlevolandAdaptee } from './dataproviders/flevolandAdaptee';
+import { NOSAdaptee } from './dataproviders/nosAdaptee';
 
 @Module({
   imports: [HttpModule],
   controllers: [ArticlesController],
-  providers: [ArticlesService],
+  providers: [ArticlesService, FlevolandAdaptee, NOSAdaptee],
 })
 export class ArticlesModule {}
